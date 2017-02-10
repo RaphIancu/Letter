369471c57c39dd5e1753715ff72266ff9797bd98
raphael <rififi3@hotmail.fr>
2017-02-10 09:47:17 +0100

    Ajout classe joueur

---
+++ LetterGame/src/main/java/fr/esiea/unique/binome/name/dictionary/Joueur.java
@@ -0,0 +1,59 @@
+/**
+ * 
+ * @author Raphael
+ *
+ */
+
+package fr.esiea.unique.binome.name.dictionary;
+
+import java.util.LinkedList;
+
+public class Joueur {
+	private String pseudo;
+	private int nbMot;
+	private LinkedList<String> joueurMots;
+	private boolean gagnant;
+	
+	public Joueur() {
+		this.pseudo = "";
+		this.nbMot = 0;
+		this.joueurMots = new LinkedList<String>();
+		this.gagnant = false;
+	}
+	
+	public String getPseudo() {
+		return pseudo;
+	}
+	
+	public void setPseudo(String pseudo) {
+		this.pseudo = pseudo;
+	}
+	
+	public int getNbMot() {
+		return nbMot;
+	}
+	
+	public void setNbMot() {
+		this.nbMot = joueurMots.size();
+	}
+	
+	public LinkedList<String> getJoueurMots() {
+		return joueurMots;
+	}
+	
+	public String getJoueurMotsToString() {
+		return joueurMots.toString();
+	}
+	
+	public void setJoueurMots(String unMot) {
+		this.joueurMots.add(unMot);
+	}
+	
+	public boolean getGagnant() {
+		return gagnant;
+	}
+	
+	public void setGagnant(boolean gagne) {
+		this.gagnant = gagne;
+	}
+}
