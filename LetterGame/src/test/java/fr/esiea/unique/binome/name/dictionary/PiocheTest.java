35c2e9046c92cb7cf9e3afd78f93682fa5a4218c
raphael <rififi3@hotmail.fr>
2017-02-17 13:42:48 +0100

    Methode Pioche de test

---
+++ LetterGame/src/test/java/fr/esiea/unique/binome/name/dictionary/PiocheTest.java
@@ -0,0 +1,30 @@
+package fr.esiea.unique.binome.name.dictionary;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * 
+ * @author Florent
+ *
+ */
+
+public class PiocheTest {
+	private Pioche pioche = new Pioche();
+	
+	@Test
+	public void testMeilleurLettre() {
+		char l1 = 'a';
+		char l2 = 'b';
+		assertTrue(pioche.meilleurLettre(l1, l2));
+		assertFalse(pioche.meilleurLettre(l2, l1));
+		assertTrue(pioche.meilleurLettre(l1, l1));
+	}
+	
+	@Test
+	public void testLetterRandom() {
+		
+	}
+}
