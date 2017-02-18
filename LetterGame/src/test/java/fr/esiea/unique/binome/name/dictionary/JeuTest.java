face3d059bb7802451c500fe6fb3d23c80fc1354
raph <rififi3@hotmail.fr>
2017-02-18 16:48:23 +0100

    Ajout du test des méthodes

--- LetterGame/src/test/java/fr/esiea/unique/binome/name/dictionary/JeuTest.java
+++ LetterGame/src/test/java/fr/esiea/unique/binome/name/dictionary/JeuTest.java
@@ -1,11 +1,7 @@
 package fr.esiea.unique.binome.name.dictionary;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertTrue;
-
-import java.util.LinkedList;
-import java.util.List;
 
 import org.junit.Test;
 
@@ -16,13 +12,9 @@ import org.junit.Test;
  */
 
 public class JeuTest {
-	private Pioche pioche = new Pioche();
-	private List<Character> lettreSurTable = new LinkedList<Character>();
 	private Joueur j1 = new Joueur();
 	private Joueur j2 = new Joueur();
 	private Joueur ia = new Joueur();
-	private Dico dico = new Dico();
-	private boolean aJouer = false;
 	private Jeu jeu = new Jeu();
 	
 	@Test
@@ -63,31 +55,51 @@ public class JeuTest {
 
 	@Test
 	public void testFaitUnMot() {
-		
+		jeu.faitUnMot(j1, j2);
+		assertTrue(jeu.getLettreSurTable().size() >= 1);
 	}
 
 	@Test
 	public void testMotPossible() {
-		
+		jeu.getLettreSurTable().add('t');
+		jeu.getLettreSurTable().add('e');
+		jeu.getLettreSurTable().add('s');
+		jeu.getLettreSurTable().add('t');
+		jeu.motPossible("test");
+		assertTrue(true);
 	}
 
 	@Test
 	public void testFaitUnMotAvecUnMot() {
-		
+		jeu.getLettreSurTable().add('a');
+		jeu.getLettreSurTable().add('b');
+		jeu.getLettreSurTable().add('e');
+		jeu.getLettreSurTable().add('r');
+		j1.getJoueurMots().add("ration");
+		jeu.faitUnMotAvecUnMot(j1, j2);
+		assertTrue(jeu.getLettreSurTable().size() >= 1);
 	}
 
 	@Test
 	public void testFaitUnMotAvecDeuxMot() {
-		
+		j1.getJoueurMots().add("aber");
+		j1.getJoueurMots().add("ration");
+		jeu.faitUnMotAvecUnMot(j1, j2);
+		assertTrue(jeu.getLettreSurTable().size() >= 1);
 	}
 
 	@Test
 	public void testRemoveUnMot() {
-		
+		j1.getJoueurMots().add("test");
+		jeu.removeUnMot(j1, j2, "test");
+		assertTrue(j1.getJoueurMots().size() == 0);
 	}
 
 	@Test
 	public void testTestGagnant() {
-		
+		for(int i = 0; i < 9; i++) {
+			j1.getJoueurMots().add("test"+i);
+		}
+		assertTrue(j1.getGagnant());
 	}
 }
