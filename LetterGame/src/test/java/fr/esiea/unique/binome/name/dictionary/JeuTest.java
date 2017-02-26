108b14d712999e12fe785e60a7f8fc555041abe9
raph <rififi3@hotmail.fr>
2017-02-26 14:47:55 +0100

    Ajout des tests + corrections

--- LetterGame/src/test/java/fr/esiea/unique/binome/name/dictionary/JeuTest.java
+++ LetterGame/src/test/java/fr/esiea/unique/binome/name/dictionary/JeuTest.java
@@ -2,8 +2,10 @@ package fr.esiea.unique.binome.name.dictionary;
 
 import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
 
 import org.junit.Test;
+
 
 /**
  * 
@@ -16,19 +18,21 @@ public class JeuTest {
 	private Joueur j2 = new Joueur();
 	private Joueur ia = new Joueur();
 	private Jeu jeu = new Jeu();
+	private Dico dico = new Dico();
 	
 	@Test
 	public void testInitPseudos() {
-		jeu.initPseudos();
+		j1.setPseudo("pseudo1");
+		j2.setPseudo("pseudo1");
 		assertNotEquals(j1.getPseudo(), null);
 		assertNotEquals(j2.getPseudo(), null);
 	}
 
 	@Test
 	public void testInitPseudo() {
-		jeu.initPseudo();
+		j1.setPseudo("pseudo1");
+		ia.setPseudo("pseudoIA");
 		assertNotEquals(j1.getPseudo(), null);
-		assertNotEquals(ia.getPseudo(), null);
 		assertNotEquals(ia.getPseudo(), null);
 	}
 	
@@ -49,14 +53,23 @@ public class JeuTest {
 
 	@Test
 	public void testPioche() {
-		jeu.pioche();
+		jeu.pioche(j1);
 		assertTrue(jeu.getLettreSurTable().size() >= 1);
 	}
 
 	@Test
 	public void testFaitUnMot() {
-		jeu.faitUnMot(j1, j2);
-		assertTrue(jeu.getLettreSurTable().size() >= 1);
+		jeu.getLettreSurTable().add('t');
+		jeu.getLettreSurTable().add('e');
+		jeu.getLettreSurTable().add('s');
+		jeu.getLettreSurTable().add('t');
+		String mot = "test";
+		if(jeu.motPossible(mot, j1)) {
+			j1.setJoueurMots(mot);
+			assertTrue(j1.getJoueurMots().size() == 1);
+		} else {
+			assertFalse(j1.getJoueurMots().size() == 1);
+		}
 	}
 
 	@Test
@@ -65,8 +78,7 @@ public class JeuTest {
 		jeu.getLettreSurTable().add('e');
 		jeu.getLettreSurTable().add('s');
 		jeu.getLettreSurTable().add('t');
-		jeu.motPossible("test");
-		assertTrue(true);
+		assertTrue(jeu.motPossible("test", j1));
 	}
 
 	@Test
@@ -76,16 +88,33 @@ public class JeuTest {
 		jeu.getLettreSurTable().add('e');
 		jeu.getLettreSurTable().add('r');
 		j1.getJoueurMots().add("ration");
-		jeu.faitUnMotAvecUnMot(j1, j2);
-		assertTrue(jeu.getLettreSurTable().size() >= 1);
+		for(int i = 0; i < j1.getJoueurMots().get(0).length(); i++) {
+			jeu.getLettreSurTable().add(j1.getJoueurMots().get(0).charAt(i));
+		}
+		String mot = "aberration";
+		if(jeu.motPossible(mot, j1)) {
+			j1.getJoueurMots().remove("ration");
+			j1.setJoueurMots(mot);
+		}
+		assertTrue(j1.getJoueurMots().get(0).equals("aberration"));
 	}
 
 	@Test
 	public void testFaitUnMotAvecDeuxMot() {
-		j1.getJoueurMots().add("aber");
-		j1.getJoueurMots().add("ration");
-		jeu.faitUnMotAvecUnMot(j1, j2);
-		assertTrue(jeu.getLettreSurTable().size() >= 1);
+		j1.setJoueurMots("aber");
+		j2.setJoueurMots("ration");
+		String mot = "";
+		for(int i = 0; i < j1.getJoueurMots().size(); i++) {
+			for(int j = 0; j < j2.getJoueurMots().size(); j++) {
+				 mot = j1.getJoueurMots().get(i).concat(j2.getJoueurMots().get(j));
+				if(dico.motExiste(mot)) {
+					j1.getJoueurMots().remove(j1.getJoueurMots().get(i));
+					j2.getJoueurMots().remove(j2.getJoueurMots().get(j));
+					j1.setJoueurMots(mot);
+				}
+			}
+		}
+		assertTrue(j1.getJoueurMots().get(0).equals(mot));
 	}
 
 	@Test
@@ -97,9 +126,40 @@ public class JeuTest {
 
 	@Test
 	public void testTestGagnant() {
-		for(int i = 0; i < 9; i++) {
+		for(int i = 0; i <= 9; i++) {
 			j1.getJoueurMots().add("test"+i);
 		}
-		assertTrue(j1.getGagnant());
+		jeu.testGagnant(j1);
+		assertTrue(j1.getGagnant() == true);
+	}
+	
+	@Test
+	public void motPossibleParIA() {
+		
+	}
+
+	@Test
+	public void faitUnMotIA() {
+		
+	}
+
+	@Test
+	public void estLeMeilleurMot() {
+		
+	}
+
+	@Test
+	public void motAvecUnMotIA() {
+		
+	}
+
+	@Test
+	public void motAvecDeuxMotsIA() {
+		
+	}
+
+	@Test
+	public void chercherUnMotAvecDeuxMotsIA() {
+		
 	}
 }
